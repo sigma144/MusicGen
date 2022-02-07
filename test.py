fefff15ba308a913cf4d78b7299a99d42e022c8a
Brian Robinson <sigma-144@hotmail.com>
2022-02-07 14:22:08 -0700

    Testing chord gen

--- test.py
+++ test.py
@@ -6,6 +6,19 @@ import random
 import accomp
 import rhythmgen
 from rhythmgen import RhythmGen
+from samples import Samples
 
-
+#Arpeggio + Bass
+testmusic = Music(tempo = 85)
+testtrack = Track(instrument = 49)
+chords = Samples().chord_prog_generator_scale(8) * 4
+testtrack.notes = accomp.accomp_from_chords(chords, style=accomp.CHORDS)
+testmusic.tracks.append(testtrack)
+arptrack = Track(instrument = 46)
+arptrack.notes = accomp.accomp_from_chords(chords, style=accomp.ARPEGGIO)
+testmusic.tracks.append(arptrack)
+basstrack = Track(instrument = 42)
+basstrack.notes = accomp.accomp_from_chords(chords, style=accomp.BASS)
+testmusic.tracks.append(basstrack)
+create_MIDI(testmusic, 'chordgen.mid')
 
