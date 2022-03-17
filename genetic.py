c49b0c65533d5406a9ce88d3fe035606dec40dd6
Xin Zhao <xinzhao0822@gmail.com>
2022-03-17 13:55:37 -0600

    add melody type evaluator

--- genetic.py
+++ genetic.py
@@ -1,4 +1,5 @@
-from geneticUtil import geneticUtil as util
+from geneticUtil import geneticUtil
+
 
 '''
 For the genetc algorithm we will evaaluate the songs by eithe 4 measures or 8 measures
@@ -6,46 +7,47 @@ For the genetc algorithm we will evaaluate the songs by eithe 4 measures or 8 me
 
 NOTE_RANAGE = 49
 
-class genetic:
+class Genetic:
 
     def __init__(self, smaple) -> None:
         self.population = []
+        self.util = geneticUtil()
 
-    def mutation():
+    def mutation(self):
         pass
 
-    def crossover():
+    def crossover(self):
         pass
 
     # TBD
-    def getChords():
+    def getChords(self):
         pass
 
     # get 8 measures of rhythm
-    def getRhythm():
+    def getRhythm(self):
         pass
 
     # measures are passed in as an 2d array
     # each subarray is a measure, a measure is an array of tuples(notes), each tuple has (pitch, time) information
-    def rhythmEvaluation(measures):
+    def rhythmEvaluation(self, measures):
         pass
 
     # get 8 meaasures of melody
-    def getMelody():
+    def getMelody(self):
         pass
 
     # extract features of a piece of melody
-    def melodyFeatureExtraction():
+    def melodyFeatureExtraction(self):
         pass
 
     # measures how often repating measures occur in this piece
     # if the same measure occurs often in this piece means this piece is more self similar 
-    def melodySelfSimilarity(measures):
+    def melodySelfSimilarity(self, measures):
         measureDict = {}
         totalNotes = 0
         for measure in measures:
             totalNotes += len(measure)
-            measureEncode = util.getMeasureEncode(measure)
+            measureEncode = self.util.getMeasureEncode(measure)
             if measureEncode in measureDict:
                 measureDict[measureEncode] += 1
             else:
@@ -57,27 +59,45 @@ class genetic:
 
     # there are 5 representations of melody shapes
     # Flat, Rising, Falling, TopArc, and BottomArc
-    def melodyShape(measures):
-        pass      
+    def melodyShape(self, measures):
+        length = self.util.getNotesLength(measures)
+        for measure in measures:
+            length += len(measure)
+        m_max = NOTE_RANAGE / length
 
-    def melodyLinearity(measures):
+        # Divide the 8 measure into 4 sub measures
+        sectionOne = measures[:2]
+        sectionTwo = measures[2:4]
+        sectionThree = measures[4:6]
+        sectionFour = measures[6:]
+        modelOne = self.util.calcRegression(sectionOne)
+        modelTwo = self.util.calcRegression(sectionTwo)
+        modelThree = self.util.calcRegression(sectionThree)
+        modelFour = self.util.calcRegression(sectionFour)
+        typeOne = self.util.getTypeViaMSE(modelOne)
+        typeTwo = self.util.getTypeViaMSE(modelTwo)
+        typeThree = self.util.getTypeViaMSE(modelThree)
+        typeFour = self.util.getTypeViaMSE(modelFour)
+        return [typeOne, typeTwo, typeThree, typeFour]
+
+    def melodyLinearity(self, measures):
         pass
 
-    def melodyKeyPrevalence(measures):
+    def melodyKeyPrevalence(self, measures):
         pass
 
-    def melodyRangeOfPitch(measures):
+    def melodyRangeOfPitch(self, measures):
         pass
 
-    def melodyIntervalClassPrevalence(measures):
+    def melodyIntervalClassPrevalence(self, measures):
         pass  
 
-    def melodyEvaluation(measures):
+    def melodyEvaluation(self, measures):
         pass
 
-    def run():
+    def run(self):
         pass
 
     # TBD
-    def chordsEvaluation():
+    def chordsEvaluation(self):
         pass
